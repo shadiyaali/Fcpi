7e9631f1ee34bf5f16ceea71567b87fbb436fac6
shadiya <118953383+shadiyaali@users.noreply.github.com>
2024-02-23 16:51:48 +0530

    otp-1

--- src/Components/User/Dashboard/Otp.jsx
+++ src/Components/User/Dashboard/Otp.jsx
@@ -8,7 +8,7 @@ const Otp = () => {
     return (
         <div className="w-full h-full bg-[#f4f4f4]">
 
-            <div className="p-[15.9rem] pl-[40rem] ">
+            <div className="p-[13.7rem] pl-[40rem] ">
            <div className="bg-white w-[60%] pb-12  shadow-sm-light rounded-[16px]  "> 
            <div className=" pt-12 pl-40 pr-20">
             <img src={Log} alt="" className="w-[80%]"/></div>
@@ -19,7 +19,7 @@ const Otp = () => {
             <p className="text-[color:var(--Black,#222)] text-[24px] not-italic font-semibold leading-[normal]">Please Enter OTP</p>
             <p className="text-[color:var(--Gray,#58585A)] pt-2 text-center text-[16px] not-italic font-normal leading-[20px]">We have sent the verification code to<br/>
 your email address</p>
-<div className="flex pl-24 gap-4 pt-8">
+<div className="flex pl-28 gap-4 pt-8">
    <div className="border border-[#D9D9D9] px-10 py-8 rounded-[6px]"></div>
    <div className="border border-[#D9D9D9] px-10 py-8 rounded-[6px]"></div>
    <div className="border border-[#D9D9D9] px-10 py-8 rounded-[6px]"></div>
@@ -29,7 +29,7 @@ your email address</p>
   Didn’t receive the OTP? <span style={{ color: '#1AB78D'  }}>Resend</span>
 </p>
 <div className="pt-8 ">
-<button className="bg-[#00549A] px-20 py-3 rounded-[6px]">
+<button className="bg-[#00549A] px-24 py-3 rounded-[6px]">
     <p className=" text-[color:var(--ffffff,#FFF)] text-center text-[18px] not-italic font-semibold leading-[normal]">Verify</p>
 </button>
 </div>
