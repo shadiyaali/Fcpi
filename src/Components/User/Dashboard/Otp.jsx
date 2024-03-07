cdfa62f688debffb94c972952222cbf8200f302b
shadiya <118953383+shadiyaali@users.noreply.github.com>
2024-03-07 17:52:51 +0530

    reg

--- src/Components/User/Dashboard/Otp.jsx
+++ src/Components/User/Dashboard/Otp.jsx
@@ -1,50 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from 'react-router-dom';
 import Log from "../../../assets/images/Group.svg";
 import "./Otp.css"
 
 const Otp = () => {
+    const inputRefs = useRef([]);
 
+    const handleChange = (e, index) => {
+        const value = e.target.value;
+        if (value.length === 1 && index < inputRefs.current.length - 1) {
+            inputRefs.current[index + 1].focus();
+        }
+    };
 
     return (
         <div className="w-full h-full bg-[#f4f4f4]">
-
             <div className="p-[13.7rem] pl-[40rem] ">
-           <div className="bg-white w-[60%] pb-12  shadow-sm-light rounded-[16px]  "> 
-           <div className=" pt-12 pl-40 pr-20">
-            <img src={Log} alt="" className="w-[80%]"/></div>
-            
-         
-           <hr className="mt-10 w-[100%]"/>
-           <div className="text-center mt-6">
-            <p className="text-[color:var(--Black,#222)] text-[24px] not-italic font-semibold leading-[normal]">Please Enter OTP</p>
-            <p className="text-[color:var(--Gray,#58585A)] pt-2 text-center text-[16px] not-italic font-normal leading-[20px]">We have sent the verification code to<br/>
-your email address</p>
-<div className="flex  justify-center gap-4 pt-8">
-  <input type="number" className="input-field" />
-  <input type="number" className="input-field" />
-  <input type="number" className="input-field" />
-  <input type="number" className="input-field" />
-</div>
-
-
-<p className="text-[color:var(--Gray,#58585A)] text-[18px] pt-4 not-italic font-medium leading-[24px]">
-  Didn’t receive the OTP? <span style={{ color: '#1AB78D'  }}>Resend</span>
-</p>
-<div className="pt-8 ">
-<Link to="/login">
-<button className="bg-[#00549A] px-24 py-3 rounded-[6px]">
-    <p className=" text-[color:var(--ffffff,#FFF)] text-center text-[18px] not-italic font-semibold leading-[normal]">Verify</p>
-</button>
-</Link>
-</div>
-           </div>
-        </div>
-        </div>
+                <div className="bg-white w-[60%] pb-12  shadow-sm-light rounded-[16px]  ">
+                    <div className=" pt-12 pl-40 pr-20">
+                        <img src={Log} alt="" className="w-[80%]" />
+                    </div>
+                    <hr className="mt-10 w-[100%]" />
+                    <div className="text-center mt-6">
+                        <p className="text-[color:var(--Black,#222)] text-[24px] not-italic font-semibold leading-[normal]">Please Enter OTP</p>
+                        <p className="text-[color:var(--Gray,#58585A)] pt-2 text-center text-[16px] not-italic font-normal leading-[20px]">We have sent the verification code to<br />
+                            your email address</p>
+                        <div className="flex  justify-center gap-4 pt-8">
+                            {[...Array(4)].map((_, index) => (
+                                <input
+                                    key={index}
+                                    type="number"
+                                    className="input-field"
+                                    ref={el => (inputRefs.current[index] = el)}
+                                    maxLength={1}
+                                    onChange={e => handleChange(e, index)}
+                                />
+                            ))}
+                        </div>
+                        <p className="text-[color:var(--Gray,#58585A)] text-[18px] pt-4 not-italic font-medium leading-[24px]">
+                            Didn’t receive the OTP? <span style={{ color: '#1AB78D' }}>Resend</span>
+                        </p>
+                        <div className="pt-8 ">
+                            <Link to="/login">
+                                <button className="bg-[#00549A] px-24 py-3 rounded-[6px]">
+                                    <p className=" text-[color:var(--ffffff,#FFF)] text-center text-[18px] not-italic font-semibold leading-[normal]">Verify</p>
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Otp;
-
-
--- src/Components/User/Register/Register.jsx
+++ src/Components/User/Register/Register.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import reg from "../../../assets/images/reg.png";
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom"
+import axios from 'axios';
+import { BASE_URL } from "../../../Utils/Config";
+import { toast, Toaster } from "react-hot-toast";
+import { convertLength } from "@mui/material/styles/cssUtils";
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        first_name: '',
+        last_name: '',
+        phone_number: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const {
+        first_name,
+        last_name,
+        phone_number,
+        email,
+        password,
+        password2
+
+
+    } = formData;
+    const handleChange = (e, fieldName) => {
+        setFormData({
+            ...formData,
+            [fieldName]: e.target.value  
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        console.log(BASE_URL)
+        e.preventDefault();
+
+        if (password !== password2) {
+            toast.error("Passwords do not match.");
+            return;
+        }
+
+        try {
+            const response = await axios.post(`${BASE_URL}/accounts/register/`, {
+                first_name,
+                last_name,
+                email,
+                phone_number,
+                password,
+
+            });
+
+            if (response.status === 201) {
+                toast.success("Registration successful. Please activate your account.");
+            } else {
+                toast.error("Something went wrong during registration.");
+            }
+        } catch (error) {
+            console.error(error);
+            toast.error("An error occurred during registration.");
+        }
+    };
 
 
     return (
@@ -15,40 +73,82 @@ const Register = () => {
                             <div className="text-start  p-12">
                                 <p className="text-[#222] text-[34px] not-italic font-semibold leading-[36px]"  >User Registration</p>
                                 <p className="text-[#58585A] pt-4 text-[19px] not-italic font-normal leading-[28px]" >Register Now and Get Access to Our Contents...</p>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="pt-6">
+                                        <input
+                                            type="text"
+                                            className="border border-gray-300 rounded-[30px] w-[541px] px-[20px] py-4"
+                                            placeholder="First Name"
+                                            value={first_name}
+                                            onChange={(e) => handleChange(e, 'first_name')}
+                                        />
 
-                                <div className="pt-6">
-                                    <input type="text" className="border border-gray-300 rounded-[30px] w-[541px] px-[20px] py-4  " placeholder="First Name" />
-                                </div>
+                                    </div>
 
-                                <div className="pt-6">
-                                    <input type="text" className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full" placeholder="Last Name" />
-                                </div>
+                                    <div className="pt-6">
+                                        <input
+                                            type="text"
+                                            className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full"
+                                            placeholder="Last Name"
+                                            value={last_name}
+                                            onChange={(e) => handleChange(e, 'last_name')}
+                                        />
 
-                                <div className="pt-6">
-                                    <input type="text" className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full" placeholder="Phone Number" />
-                                </div>
-                                <div className="pt-6">
-                                    <input type="text" className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full" placeholder="Email" />
-                                </div>
-                                <div className="pt-6">
-                                    <input type="text" className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full" placeholder="Enter your password" />
-                                </div>
-                                <div className="pt-6">
-                                    <input type="text" className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full" placeholder="Re-Enter your password" />
-                                </div>
-                                <div className="text-end pt-6">
-                                    <Link to="/otp">
-                                    <button className="bg-[#1AB78D] px-16 rounded-[30px] py-4 ">
-                                        <p className="text-white text-[17px] not-italic font-semibold leading-[normal]" >Register Now</p>
-                                    </button>
-                                    </Link>
-                                </div>
+                                    </div>
 
+                                    <div className="pt-6">
+                                        <input
+                                            type="text"
+                                            className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full"
+                                            placeholder="Phone Number"
+                                            value={phone_number}
+                                            onChange={(e) => handleChange(e, 'phone_number')}
+                                        />
 
+                                    </div>
+                                    <div className="pt-6">
+                                        <input
+                                            type="email"
+                                            className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full"
+                                            placeholder="Email"
+                                            value={email}
+                                            onChange={(e) => handleChange(e, 'email')}
+                                        />
+
+                                    </div>
+                                    <div className="pt-6">
+                                        <input
+                                            type="password"
+                                            className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full"
+                                            placeholder="Enter your password"
+                                            value={password}
+                                            onChange={(e) => handleChange(e, 'password')}
+                                        />
+
+                                    </div>
+                                    <div className="pt-6">
+                                        <input
+                                            type="password"
+                                            className="border border-gray-300 rounded-[30px] px-[20px] py-4 w-full"
+                                            placeholder="Re-Enter your password"
+                                            value={password2}
+                                            onChange={(e) => handleChange(e, 'password2')}  
+                                        />
+
+                                    </div>
+                                    <div className="text-end pt-6">
+                                    
+                                            <button type="submit" className="bg-[#1AB78D] px-16 rounded-[30px] py-4 ">
+                                                <p className="text-white text-[17px] not-italic font-semibold leading-[normal]" >Register Now</p>
+                                            </button>
+                                        
+                                    </div>
+
+                                </form>
                             </div>
                         </div>
-                        <div className="  p-12">
-                            <img src={reg} alt=""  className="rounded-[30px] h-[98%] w-[821px]"/>
+                        <div className="p-12">
+                            <img src={reg} alt="" className="rounded-[30px] h-[98%] w-[821px]" />
                         </div>
                     </div>
 
